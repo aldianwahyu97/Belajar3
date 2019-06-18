7a6e1e3055388dda5d6af8650148f23b3514f604
unknown <aldianwahyuseptiadi1997@gmail.com>
2019-06-18 21:48:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/belajar/Belajar2.java
@@ -0,0 +1,27 @@
+package com.example.belajar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Belajar2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belajar2);
+        getSupportActionBar().setTitle("Pindah");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}
